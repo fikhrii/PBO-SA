22d3b946927096e3ee3bfdb2eacc795ecb90a97d
fikhrii <101464839+fikhrii@users.noreply.github.com>
2022-03-14 20:40:57 +0700

    Add files via upload

---
+++ Minggu1/konstanta.php
@@ -0,0 +1,6 @@
+<?
+define ("NAMA", "Fikhri Wahyu Ramadhan");
+define ("NILAI", 90);
+echo "Nama : " . NAMA;
+echo "<br>Nilai : " . NILAI;
+?>
---
+++ Minggu1/variabel.php
@@ -0,0 +1,6 @@
+<?php
+$nim = "A11.2018.11309";
+$nama = 'Fikhri Wahyu Ramadhan';
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama";
+?>
